6777e9412865c0b2875e3b694406eadfeb05adef
Joey Tepperman <jmichaelt98@gmail.com>
2018-11-21 19:39:13 -0500

    Add ILC Room capabilities to geoff

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,27 @@
-FROM python:3.6
+FROM ubuntu:16.04
+
+# Install Dependencies
+RUN apt-get update
+RUN apt-get install -y software-properties-common vim
+RUN add-apt-repository ppa:jonathonf/python-3.6
+RUN apt-get update
+RUN apt-get install -y build-essential python3.6 python3.6-dev python3-pip python3.6-venv
+RUN apt-get install -y wget
+RUN apt-get install firefox --yes
+
+# Update pip
+RUN python3.6 -m pip install pip --upgrade
+RUN python3.6 -m pip install wheel
+
+# Initialize app
 WORKDIR /app
 COPY requirements.txt /app
 RUN pip install -r requirements.txt
+
+# Download geckodriver
+RUN wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
+RUN sh -c 'tar -x geckodriver -zf geckodriver-v0.23.0-linux64.tar.gz -O > /usr/bin/geckodriver'
+RUN chmod +x /usr/bin/geckodriver
+RUN rm geckodriver-v0.23.0-linux64.tar.gz
 COPY . /app
-CMD python bot.py
+CMD python3.6 bot.py
--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
+# Load environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 import re
 import time
-from commands import help_me, get_gif
-from dotenv import load_dotenv
-from slackclient import SlackClient
+from commands import help_me, get_gif, rooms
 
-# Load environment variables
-load_dotenv()
+from slackclient import SlackClient
 
 # initialize Slack Client
 slack_client = SlackClient(os.environ.get('BOT_OAUTH_TOKEN'))
@@ -19,7 +20,10 @@ giphy_api_key = os.environ.get("BOT_GIPHY_API_KEY")
 RTM_READ_DELAY = 1
 HELP_COMMAND = "help"
 GIF_COMMAND = "gif"
+ROOMS_COMMAND = "rooms"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+DIBS_USERNAME = os.environ.get('DIBS_USERNAME')
+DIBS_PASSWORD = os.environ.get('DIBS_PASSWORD')
 
 
 def parse_bot_commands(slack_events):
@@ -39,7 +43,7 @@ def parse_direct_mention(message_text):
 
 def handle_command(command, channel):
     # Default response is help text for the user
-    default_response = f"Howdy :sad_cowboy:! I'm geoff, the QMIND Bot that makes your lives easier. Try me out by using a command like *{HELP_COMMAND}* or *{GIF_COMMAND}*!"
+    default_response = f"Howdy :sad_cowboy:! I'm geoff, the QMIND Bot that makes your lives easier. Try me out by using a command like *{HELP_COMMAND}*, *{GIF_COMMAND}* or *{ROOMS_COMMAND}*!"
 
     # Finds and executes the given command, filling in response
     response = None
@@ -48,6 +52,14 @@ def handle_command(command, channel):
 
     elif command.startswith(GIF_COMMAND):
         response = get_gif.get_gif(command, giphy_api_key)
+
+    elif command.startswith(ROOMS_COMMAND):
+        slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text="Fetching rooms, this may take a second... :watch:"
+        )
+        response = rooms.check(command, DIBS_USERNAME, DIBS_PASSWORD)
 
     # Sends the response back to the channel
     slack_client.api_call(
@@ -58,16 +70,17 @@ def handle_command(command, channel):
 
 
 if __name__ == "__main__":
-    try:
-        if slack_client.rtm_connect(with_team_state=False, auto_reconnect=True):
-            print("Starter Bot connected and running!")
-            starterbot_id = slack_client.api_call("auth.test")["user_id"]
-            while(True):
+
+    if slack_client.rtm_connect(with_team_state=False, auto_reconnect=True):
+        print("QMIND Bot connected and running!")
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        while(True):
+            try:
                 command, channel = parse_bot_commands(slack_client.rtm_read())
                 if command:
                     handle_command(command, channel)
                 time.sleep(RTM_READ_DELAY)
-        else:
-            print("Connection failed. See above for exception")
-    except Exception as e:
-        print(e)
+            except Exception as e:
+                print(e)
+    else:
+        print("Connection failed. See above for exception")
